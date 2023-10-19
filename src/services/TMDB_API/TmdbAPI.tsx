cdc23a821bf5be653e9d2e8675cb1b960201e213
Juaojoao <soloqf@gmail.com>
2023-10-18 23:50:50 -0300

    Correção em uma váriavel não utilizavel

--- src/services/TMDB_API/TmdbAPI.tsx
+++ src/services/TMDB_API/TmdbAPI.tsx
@@ -1,24 +1,5 @@
 import axios from "axios";
 
-const MovieList = [
-  {
-    url: "popular",
-    text: "Populares",
-  },
-  {
-    url: "top_rated",
-    text: "Melhores Avaliados",
-  },
-  {
-    url: "upcoming",
-    text: "Em Breve",
-  },
-  {
-    url: "now_playing",
-    text: "Em Cartaz",
-  },
-];
-
 export const TMDBAuth = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
